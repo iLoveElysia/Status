c924690694502991ab4bc23f792f142514415439
FutureApple <50200605+FutureApple@users.noreply.github.com>
2020-08-05 11:36:57 +0800

    Update config.js

--- config.js
+++ config.js
@@ -14,12 +14,12 @@ window.Config = {
   ],
 
   // 是否显示监测站点的链接
-  ShowLink: false,
+  ShowLink: true,
 
   // 日志天数
   // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
   // 不过时间不要设置太长，容易卡，接口请求也容易失败
-  CountDays: 60,
+  CountDays: 90,
 
   // 导航栏菜单
   Navi: [
